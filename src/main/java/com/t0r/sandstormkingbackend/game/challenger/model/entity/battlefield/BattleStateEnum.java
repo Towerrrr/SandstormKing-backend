39fc4e0fd376e107e1e48a7c99c13950e71df95b
Towerrrr <1611993104@qq.com>
2025-12-26 23:54:55 +0800

    refactor(tmp): 战场状态机

---
+++ src/main/java/com/t0r/sandstormkingbackend/game/challenger/model/entity/battlefield/BattleStateEnum.java
@@ -0,0 +1,15 @@
+package com.t0r.sandstormkingbackend.game.challenger.model.entity.battlefield;
+
+public enum BattleStateEnum {
+
+    firstAttack,
+    triggerDefenderRestBuffs,
+    castAttack,
+    triggerAttackerBuffs,
+    checkAttackPower,
+    selectCard,
+    applyAttackDamage,
+    moveAttackerToRestZone,
+    swapAttackAndDefense,
+
+}
--- src/main/java/com/t0r/sandstormkingbackend/game/challenger/model/entity/battlefield/Battlefield.java
+++ src/main/java/com/t0r/sandstormkingbackend/game/challenger/model/entity/battlefield/Battlefield.java
@@ -6,7 +6,6 @@ import com.t0r.sandstormkingbackend.game.challenger.manager.PlayerWaitManager;
 import com.t0r.sandstormkingbackend.game.challenger.model.entity.Card;
 import com.t0r.sandstormkingbackend.game.challenger.model.entity.CardInstance;
 import com.t0r.sandstormkingbackend.game.challenger.model.entity.ChallengerPlayer;
-import com.t0r.sandstormkingbackend.game.challenger.model.entity.CupInstance;
 import com.t0r.sandstormkingbackend.game.challenger.model.entity.skill.buff.Buff;
 import com.t0r.sandstormkingbackend.game.challenger.model.entity.skill.buff.BuffCallParam;
 import com.t0r.sandstormkingbackend.game.challenger.model.entity.skill.buff.BuffTypeEnum;
@@ -51,6 +50,41 @@ public class Battlefield {
 
     private final ApplicationEventPublisher eventPublisher;
 
+    private BattleStateEnum currentState;
+
+    // region 战斗重构变量
+
+    // TODO 判断有人战斗开始直接弃光手牌？
+
+    HalfBattlefield[] halfBattlefields = new HalfBattlefield[]{
+            halfBattlefieldMap.get(startPlayerId),
+            halfBattlefieldMap.get(elsePlayerId)
+    };
+    // 0: 当前防守方, 1: 当前进攻方
+    List<LinkedList<CardInstance>> handZones = Arrays.asList(
+            halfBattlefields[0].getHandZone(),
+            halfBattlefields[1].getHandZone()
+    );
+    List<List<Consumer<BuffCallParam>>> restBuffsArray = Arrays.asList(
+            halfBattlefields[0].getRestBuffs(),
+            halfBattlefields[1].getRestBuffs()
+    );
+    List<Consumer<BuffCallParam>> nextBuffs = Arrays.asList(
+            halfBattlefields[0].getNextBuff(),
+            halfBattlefields[1].getNextBuff()
+    );
+
+    int attackerIdx = 0;
+    int defenderIdx = 1;
+    Power defenderPower = new Power(0);
+    Integer attackerPower;
+
+    Battle battle = null;
+    CardInstance attackerCard = null;
+    Power tempAttackerPower = null;
+
+    // endregion
+
     public Battlefield(String name, String currentRound, Map<Long, ChallengerPlayer> challengerPlayers,
                        ApplicationEventPublisher eventPublisher) {
         this.name = name;
@@ -62,6 +96,57 @@ public class Battlefield {
             }
         }
         this.eventPublisher = eventPublisher;
+    }
+
+    public Mono<Void> advanceBattle() {
+        return Mono.defer(() -> {
+                    log.info("当前战斗状态: {}", currentState);
+
+                    Mono<Void> nextMono = Mono.empty();
+                    switch (currentState) {
+                        case firstAttack:
+                            firstAttack();
+                            nextMono = advanceBattle();
+                            break;
+                        case triggerDefenderRestBuffs:
+                            triggerDefenderRestBuffs();
+                            nextMono = advanceBattle();
+                            break;
+                        case castAttack:
+                            castAttack();
+                            nextMono = advanceBattle();
+                            break;
+                        case triggerAttackerBuffs:
+                            triggerAttackerBuffs();
+                            nextMono = advanceBattle();
+                            break;
+                        case checkAttackPower:
+                            checkAttackPower();
+                            nextMono = advanceBattle();
+                            break;
+                        case selectCard:
+                            nextMono = selectCard(attackerIdx);
+                            advanceBattle();
+                            break;
+                        case applyAttackDamage:
+                            applyAttackDamage();
+                            nextMono = advanceBattle();
+                            break;
+                        case moveAttackerToRestZone:
+                            moveAttackerToRestZone();
+                            nextMono = advanceBattle();
+                            break;
+                        case swapAttackAndDefense:
+                            swapAttackAndDefense();
+                            nextMono = advanceBattle();
+                            break;
+                        default:
+                            throw new RuntimeException("未知的战斗状态: " + currentState);
+                    }
+
+                    return nextMono;
+                })
+                .doOnError(e -> log.error("战斗状态机异常", e));
     }
 
     public Long readyBattle(Long userId) {
@@ -103,6 +188,12 @@ public class Battlefield {
         }
 
         decideStartPlayer(challengerPlayers);
+        // 启动状态机（非阻塞）
+        advanceBattle()
+                .subscribe(
+                        nil -> log.info("战斗状态机正常完成"),
+                        error -> log.error("战斗状态机异常结束", error)
+                );
     }
 
     // region 决定开始玩家
@@ -144,121 +235,59 @@ public class Battlefield {
 
     // endregion
 
-    public void calculateBattle() {
-        HalfBattlefield[] halfBattlefields = new HalfBattlefield[]{
-                halfBattlefieldMap.get(startPlayerId),
-                halfBattlefieldMap.get(elsePlayerId)
-        };
-        // 0: 当前防守方, 1: 当前进攻方
-        List<LinkedList<CardInstance>> handZones = Arrays.asList(
-                halfBattlefields[0].getHandZone(),
-                halfBattlefields[1].getHandZone()
-        );
-        List<List<Consumer<BuffCallParam>>> restBuffsArray = Arrays.asList(
-                halfBattlefields[0].getRestBuffs(),
-                halfBattlefields[1].getRestBuffs()
-        );
-        List<Consumer<BuffCallParam>> nextBuffs = Arrays.asList(
-                halfBattlefields[0].getNextBuff(),
-                halfBattlefields[1].getNextBuff()
-        );
-
-        int attackerIdx = 0;
-        int defenderIdx = 1;
-        Power defenderPower = new Power(0);
-        Integer attackerPower;
-
-        // TODO 判断有人战斗开始直接弃光手牌？
-
-        // 最开始进攻方先放一张牌
-        Battle battle = new Battle();
-        CardInstance attackerCard = handZones.get(attackerIdx).removeFirst();
+    /**
+     * 最开始进攻方先放一张牌
+     */
+    private void firstAttack() {
+        battle = new Battle();
+        attackerCard = handZones.get(attackerIdx).removeFirst();
         battle.getAttacker().add(attackerCard);
         attackerPower = cardMap.get(attackerCard.getCardId()).getBasePower();
 
-        // 进行到最后可能先没牌的玩家是赢的，两个人都没牌的情况下可能还要进一次循环
-        while (true) {
-            if (battle.getDefender() != null) { // 跳过第一次攻击
-                // 给防守方上 休息区 BUFF
-                BuffCallParam buffCallParam =
-                        new BuffCallParam(
-                                TimeRangeEnum.CONTROL_FLAG.getValue(),
-                                defenderPower, cardMap.get(battle.getDefender().getCardId()));
-                for (Consumer<BuffCallParam> restBuff : restBuffsArray.get(defenderIdx)) {
-                    restBuff.accept(buffCallParam);
-                }
+        this.currentState = BattleStateEnum.triggerDefenderRestBuffs;
+    }
+
+    private void triggerDefenderRestBuffs() {
+        if (battle.getDefender() != null) { // 跳过第一次攻击
+            // 给防守方上 休息区 BUFF
+            BuffCallParam buffCallParam =
+                    new BuffCallParam(
+                            TimeRangeEnum.CONTROL_FLAG.getValue(),
+                            defenderPower, cardMap.get(battle.getDefender().getCardId()));
+            for (Consumer<BuffCallParam> restBuff : restBuffsArray.get(defenderIdx)) {
+                restBuff.accept(buffCallParam);
             }
+        }
 
-            // 进攻方出牌，直到攻击力 >= 防守力 或手牌用完
-            while (attackerPower < Objects.requireNonNull(defenderPower).getValue() &&
-                    !handZones.get(attackerIdx).isEmpty()) {
-                attackerCard = handZones.get(attackerIdx).removeFirst();
-                Power tempAttackerPower = new Power(cardMap.get(attackerCard.getCardId()).getBasePower());
+        this.currentState = BattleStateEnum.castAttack;
+    }
 
-                // 给进攻方上 休息区 BUFF
-                BuffCallParam buffCallParam =
-                        new BuffCallParam(
-                                TimeRangeEnum.ATTACK.getValue(),
-                                tempAttackerPower, cardMap.get(attackerCard.getCardId()));
-                for (Consumer<BuffCallParam> restBuff : restBuffsArray.get(attackerIdx)) {
-                    restBuff.accept(buffCallParam);
-                }
-                nextBuffs.get(attackerIdx).accept(buffCallParam);
+    private void castAttack() {
+        // 进攻方出牌，直到攻击力 >= 防守力 或手牌用完
+        if (attackerPower < Objects.requireNonNull(defenderPower).getValue() &&
+                !handZones.get(attackerIdx).isEmpty()) {
+            attackerCard = handZones.get(attackerIdx).removeFirst();
+            tempAttackerPower = new Power(cardMap.get(attackerCard.getCardId()).getBasePower());
 
-                // TODO “下一张卡” BUFF 技能，要结合卡的 timeRange
-
-                // 假设需要在这里调用 selectCard(int attackerIdx)
-
-                attackerPower += tempAttackerPower.getValue();
-                battle.getAttacker().add(attackerCard);
-            }
-            battleList.add(battle);
-            // 进攻方手牌耗尽攻击力依旧不足
-            if (attackerPower < defenderPower.getValue()) {
-                winnerId = defenderIdx == 0 ? startPlayerId : elsePlayerId;
-                log.info("战斗结束，进攻方无多余手牌，胜利者为 {}", winnerId);
-                break;
-            }
-            // 将上一轮的上一轮所有进攻牌放入休息区
-            if (battleList.size() >= 2) {
-                Iterator<Battle> descendingIterator = battleList.descendingIterator();
-                descendingIterator.next();
-                LinkedList<CardInstance> attacker = descendingIterator.next().getAttacker();
-
-                for (CardInstance cardInstance : attacker) {
-                    Map<String, LinkedList<CardInstance>> restZone = halfBattlefields[defenderIdx].getRestZone();
-                    Card card = cardMap.get(cardInstance.getId());
-                    if (card.getBuffType().equals(BuffTypeEnum.REST.getValue())) { // "在休息区" 技能
-                        restBuffsArray.get(defenderIdx).add(new Buff(card));
-                    }
-
-                    Move.toRestZone(cardInstance, restZone);
-                    if (restZone.size() > MAX_REST_ZONE_SIZE) {
-                        winnerId = whoIsAttackerOrDefender(attackerIdx);
-                        battleList.add(battle);
-                        log.info("战斗结束，防守方休息区溢出，胜利者为 {}", winnerId);
-                        return;
-                    }
-                }
-            }
-
-            // 交换攻守（下回合由上一轮进攻方最后一张牌做防守方）
-            if (!battle.getAttacker().isEmpty()) {
-                CardInstance lastAttacker = battle.getAttacker().getLast();
-                battle = new Battle();
-                battle.setDefender(lastAttacker);
-                defenderPower.setValue(cardMap.get(lastAttacker.getCardId()).getBasePower());
-                attackerPower = 0;
-
-                int temp = defenderIdx;
-                defenderIdx = attackerIdx;
-                attackerIdx = temp;
-            }
+            this.currentState = BattleStateEnum.triggerAttackerBuffs;
+        } else {
+            battleList.add(battle); // 记录战斗过程
+            this.currentState = BattleStateEnum.checkAttackPower;
         }
     }
 
-    private Long whoIsAttackerOrDefender(int idx) {
-        return idx == 0 ? startPlayerId : elsePlayerId;
+    private void triggerAttackerBuffs() {
+        BuffCallParam buffCallParam =
+                new BuffCallParam(
+                        TimeRangeEnum.ATTACK.getValue(),
+                        tempAttackerPower, cardMap.get(attackerCard.getCardId()));
+        for (Consumer<BuffCallParam> restBuff : restBuffsArray.get(attackerIdx)) {
+            restBuff.accept(buffCallParam);
+        }
+        nextBuffs.get(attackerIdx).accept(buffCallParam);
+        // TODO 实施“下一张卡” BUFF 技能，要结合卡的 timeRange
+
+        this.currentState = BattleStateEnum.selectCard;
     }
 
     public Mono<Void> selectCard(int attackerIdx) {
@@ -273,9 +302,82 @@ public class Battlefield {
                 new CardSelectEvent(whoIsAttackerOrDefender(attackerIdx), new CardSelectorRequest())
         );
 
-        return mono.flatMap(cardSelectorRequest -> {
-            // TODO 继续战斗的流程
-            return continueBattle(cardSelectorRequest);
-        });
+        return mono
+                .doOnSuccess(cardSelectorResponse -> {
+                    // TODO 根据选择卡响应做响应操作
+                })
+                .then()
+                .doOnSuccess(v -> this.currentState = BattleStateEnum.applyAttackDamage);
     }
+
+    private void applyAttackDamage() {
+        attackerPower += tempAttackerPower.getValue();
+        battle.getAttacker().add(attackerCard);
+
+        this.currentState = BattleStateEnum.castAttack;
+    }
+
+    /**
+     * 进攻方手牌耗尽攻击力依旧不足
+     */
+    private void checkAttackPower() {
+        if (attackerPower < defenderPower.getValue()) {
+            winnerId = defenderIdx == 0 ? startPlayerId : elsePlayerId;
+            log.info("战斗结束，进攻方无多余手牌，胜利者为 {}", winnerId);
+            // todo 跳转至结束
+        } else {
+            this.currentState = BattleStateEnum.moveAttackerToRestZone;
+        }
+    }
+
+    /**
+     * 将上一轮的上一轮所有进攻牌放入休息区
+     */
+    private void moveAttackerToRestZone() {
+        if (battleList.size() >= 2) {
+            Iterator<Battle> descendingIterator = battleList.descendingIterator();
+            descendingIterator.next();
+            LinkedList<CardInstance> attacker = descendingIterator.next().getAttacker();
+
+            for (CardInstance cardInstance : attacker) {
+                Map<String, LinkedList<CardInstance>> restZone = halfBattlefields[defenderIdx].getRestZone();
+                Card card = cardMap.get(cardInstance.getId());
+                if (card.getBuffType().equals(BuffTypeEnum.REST.getValue())) { // "在休息区" 技能
+                    restBuffsArray.get(defenderIdx).add(new Buff(card));
+                }
+
+                Move.toRestZone(cardInstance, restZone);
+                if (restZone.size() > MAX_REST_ZONE_SIZE) {
+                    winnerId = whoIsAttackerOrDefender(attackerIdx);
+                    battleList.add(battle);
+                    log.info("战斗结束，防守方休息区溢出，胜利者为 {}", winnerId);
+                    return;
+                }
+            }
+        }
+
+        this.currentState = BattleStateEnum.swapAttackAndDefense;
+    }
+
+    private void swapAttackAndDefense() {
+        if (!battle.getAttacker().isEmpty()) {
+            // 下一轮由这轮进攻方最后一张牌做防守方
+            CardInstance lastAttacker = battle.getAttacker().getLast();
+            battle = new Battle();
+            battle.setDefender(lastAttacker);
+            defenderPower.setValue(cardMap.get(lastAttacker.getCardId()).getBasePower());
+            attackerPower = 0;
+
+            int temp = defenderIdx;
+            defenderIdx = attackerIdx;
+            attackerIdx = temp;
+        }
+
+        this.currentState = BattleStateEnum.triggerDefenderRestBuffs;
+    }
+
+    private Long whoIsAttackerOrDefender(int idx) {
+        return idx == 0 ? startPlayerId : elsePlayerId;
+    }
+
 }
